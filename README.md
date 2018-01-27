5be63910a7f12793f0e2be9c8793fe0fec2b9c49
c-bennett <buddy7405>
2018-01-26 22:08:45 -0800

    First working version

--- README.md
+++ README.md
@@ -1,6 +1,17 @@
 # ProfitTrailer-NewBinanceListings
 
 Automatcally checks for new Binance listings and disables trading in ProfitTrailer.
+All newly listed coins less than 14 days old will be added to your PAIRS.properties file.
 
 # Usage
-Place ProfitTrailer-NewBinanceListings.jar in your ProfitTrailer folder. Change directory to this folder and run the jar file.
+Place ProfitTrailer-NewBinanceListings.jar in your ProfitTrailer folder.
+
+Change directory to ProfitTrailer folder and run the jar file.
+java -jar ProfitTrailer-NewBinanceListings.jar
+
+# Example console output
+2018-01-26 22:05:59 INFO Set to query https://support.binance.com/hc/en-us/sections/115000106672-New-Listings every 15 minutes
+2018-01-26 22:05:59 INFO Fetching data...
+2018-01-26 22:06:11 INFO Loaded 23 newest listings (Took 11621 ms)
+2018-01-26 22:06:39 INFO Fetching data...
+2018-01-26 22:06:39 INFO New Binance listing: IOST (Released 2018-01-24T05:47:09)
--- src/main/java/com/github/bennettca/newbinancelistings/NewBinanceListings.java
+++ src/main/java/com/github/bennettca/newbinancelistings/NewBinanceListings.java
@@ -1,5 +1,8 @@
 package com.github.bennettca.newbinancelistings;
 
+import org.apache.commons.configuration2.PropertiesConfiguration;
+import org.apache.commons.configuration2.PropertiesConfigurationLayout;
+import org.apache.commons.configuration2.ex.ConfigurationException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
@@ -8,10 +11,10 @@ import org.jsoup.select.Elements;
 import java.io.*;
 import java.time.Duration;
 import java.time.LocalDateTime;
-import java.util.LinkedHashMap;
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Properties;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
@@ -24,8 +27,11 @@ public class NewBinanceListings implements Runnable {
 
     private static final Logger LOGGER = Logger.getLogger(NewBinanceListings.class.getName());
     private static final String URL = "https://support.binance.com/hc/en-us/sections/115000106672-New-Listings";
+    private static final Pattern SYMBOL = Pattern.compile("\\((.*?)\\)");
 
     public static void main(String[] args) {
+        System.setProperty("java.util.logging.SimpleFormatter.format",
+                "%1$tY-%1$tm-%1$td %1$tH:%1$tM:%1$tS %4$s %5$s%6$s%n");
         NewBinanceListings app = new NewBinanceListings();
         app.start();
     }
@@ -34,84 +40,97 @@ public class NewBinanceListings implements Runnable {
     private ScheduledExecutorService scheduler;
 
     public NewBinanceListings() {
-        cache = new LinkedHashMap<>();
+        cache = new HashMap<>();
     }
 
     public void start() {
+        LOGGER.info("Set to query " + URL + " every 15 minutes");
         scheduler = Executors.newSingleThreadScheduledExecutor();
-        scheduler.scheduleAtFixedRate(this, 0, 15, TimeUnit.MINUTES);
+        scheduler.scheduleAtFixedRate(this, 0, 40, TimeUnit.SECONDS);
     }
 
     @Override
     public void run() {
         long start = System.currentTimeMillis();
-        Document doc = null;
-        if (!cache.isEmpty()) cache.clear();
+        boolean first = cache.isEmpty();
+        LOGGER.info("Fetching data...");
         try {
-            doc = Jsoup.connect(URL).get();
+            Document doc = Jsoup.connect(URL).get();
             Elements newListings = doc.select(".article-list-link");
             for (Element element : newListings) {
                 String title = element.text();
                 if (title.contains("Binance Lists")) {
-                    String coin = title.replaceAll("(Binance Lists|\\s+)", "");
-                    if (coin.matches("\\(A-Z+\\)")) {
-                        Matcher m = Pattern.compile("\\(A-Z+\\)").matcher(coin);
-                        while (m.find()) {
-                            coin = m.group();
-                            break;
-                        }
+                    String coin = title.replace("Binance Lists ", "").trim();
+
+                    if (coin.contains("IOS") && first) continue;
+
+                    Matcher matcher = SYMBOL.matcher(coin);
+                    if (matcher.find()) {
+                        coin = matcher.group().replaceAll("(\\(|\\))", "");
+                    } else if (coin.length() > 5) {
+                        continue;
+                    }
+                    if (cache.containsKey(coin)) {
+                        continue;
                     }
 
-                    String listingUrl = element.absUrl("href");
-                    Document listingArticle = Jsoup.connect(listingUrl).get();
+                    Document listingArticle = Jsoup.connect(element.absUrl("href")).get();
+                    Element dateElement = listingArticle.selectFirst(".meta-data time");
+                    LocalDateTime date = LocalDateTime.parse(dateElement.attr("datetime"),
+                            DateTimeFormatter.ISO_DATE_TIME);
 
-                    Element date = listingArticle.selectFirst(".meta-data time");
-                    String released = date.attr("datetime");
-
-                    LOGGER.info(String.format("\ntitle: %s\nreleased: %s", coin, released));
+                    if (!first && !cache.containsKey(coin)) {
+                        LOGGER.info("New Binance listing: " + coin + " (Released "
+                                + date.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) + ")");
+                    }
+                    cache.put(coin, date);
                 }
             }
         } catch (IOException e) {
             LOGGER.log(Level.WARNING, "Failed to query " + URL, e);
         }
+
         if (!cache.isEmpty()) {
             modifyProfitTrailerSettings();
         }
 
         long elapsed = System.currentTimeMillis() - start;
-        LOGGER.info(String.format("Queried %s.\nTook " + elapsed + " ms", URL));
+        if (first) {
+            LOGGER.info(String.format("Loaded " + cache.size() + " newest listings (Took " + elapsed + " ms)", URL));
+        }
     }
 
     private void modifyProfitTrailerSettings() {
         File pairsFile = new File("trading", "PAIRS.properties");
+        PropertiesConfiguration config = new PropertiesConfiguration();
+        PropertiesConfigurationLayout layout = new PropertiesConfigurationLayout();
 
-        Properties props = new Properties();
         try {
-            props.load(new FileInputStream(pairsFile));
-        } catch (IOException e) {
+            layout.load(config, new InputStreamReader(new FileInputStream(pairsFile)));
+        } catch (ConfigurationException | FileNotFoundException e) {
             LOGGER.log(Level.WARNING, "Failed to load " + pairsFile.getName(), e);
-            return;
         }
+        config.setLayout(layout);
 
-        String market = props.getProperty("MARKET");
-        if (market == null) {
+        if (!config.containsKey("MARKET")) {
             LOGGER.warning("No market set in " + pairsFile.getName());
             return;
         }
+        String market = (String) config.getProperty("MARKET");
 
         LocalDateTime now = LocalDateTime.now();
         for (Entry<String, LocalDateTime> entry : cache.entrySet()) {
-            if (Duration.between(now, entry.getValue()).abs().toDays() <= 7) {
+            if (Duration.between(now, entry.getValue()).abs().toDays() <= 14) {
                 String propsKey = entry.getKey() + market + "_trading_enabled";
-                if (!props.containsKey(propsKey)) {
-                    props.setProperty(propsKey, "false");
+                if (!config.containsKey(propsKey)) {
+                    config.setProperty(propsKey, "false");
                 }
             }
         }
 
         try {
-            props.store(new FileOutputStream(pairsFile), null);
-        } catch (IOException e) {
+            config.write(new FileWriter(pairsFile, false));
+        } catch (ConfigurationException | IOException e) {
             LOGGER.log(Level.WARNING, "Failed to save " + pairsFile.getName(), e);
         }
     }
